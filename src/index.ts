39873fa0047f954883e16e2b6f7b926739a3052e
zakariaemse <81692956+zakariaemse@users.noreply.github.com>
2021-06-13 12:30:46 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,217 @@
-/// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
-
-// You can write your WorkAdventure script here, if any.
+@@ -4,4 +4,215 @@
 // The "WA" global object is available from anywhere.
 
 console.log('Script started successfully');
 WA.openCoWebSite('https://workadventu.re');
+WA.sendChatMessage("Bienvenue sur WorkAdventures, n'hésitez pas à soliciter le staff pour toutes vos questions. Nous esperons que vous allez passer un super moment en notre compagnie.\n Vous pouvez fermer cette fenêtre.", "L'équipe HTH");
+
+
+let helloWorldPopup: any = WA.onEnterZone('sport1', () => {
+  WA.openPopup('popup_sport1', 'Bienvenue dans la salle solo : sport !', [{
+      label: "Merci !",
+      className: "primary",
+      callback: (popup) => {
+          // Close the popup when the "Close" button is pressed.
+          popup.close();
+      }
+  }])
+})
+
+WA.onLeaveZone('sport1', () => {
+  helloWorldPopup.close();
+});
+
+let helloWorldPopup2: any = WA.onEnterZone('sport1', () => {
+    WA.openPopup('popup_concert', 'Bienvenue dans la salle de concert !', [{
+        label: "Merci !",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }])
+  })
+  
+  WA.onLeaveZone('sport1', () => {
+    helloWorldPopup2.close();
+  });
+
+  let helloWorldPopup3: any = WA.onEnterZone('sport1', () => {
+    WA.openPopup('popup_sport1', 'Bienvenue dans la salle solo : sport !', [{
+        label: "Merci !",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }])
+  })
+  
+  WA.onLeaveZone('sport1', () => {
+    helloWorldPopup.close();
+  });
+
+  let helloWorldPopup4: any = WA.onEnterZone('sport1', () => {
+    WA.openPopup('popup_sport1', 'Bienvenue dans la salle solo : sport !', [{
+        label: "Merci !",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }])
+  })
+  
+  WA.onLeaveZone('sport1', () => {
+    helloWorldPopup.close();
+  });
+
+  let helloWorldPopup5: any = WA.onEnterZone('sport1', () => {
+    WA.openPopup('popup_sport1', 'Bienvenue dans la salle solo : sport !', [{
+        label: "Merci !",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }])
+  })
+  
+  WA.onLeaveZone('sport1', () => {
+    helloWorldPopup.close();
+  });
+
+  let helloWorldPopup6: any = WA.onEnterZone('sport1', () => {
+    WA.openPopup('popup_sport1', 'Bienvenue dans la salle solo : sport !', [{
+        label: "Merci !",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }])
+  })
+  
+  WA.onLeaveZone('sport1', () => {
+    helloWorldPopup.close();
+  });
+
+  let helloWorldPopup7: any = WA.onEnterZone('sport1', () => {
+    WA.openPopup('popup_sport1', 'Bienvenue dans la salle solo : sport !', [{
+        label: "Merci !",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }])
+  })
+  
+  WA.onLeaveZone('sport1', () => {
+    helloWorldPopup.close();
+  });
+
+  let helloWorldPopup8: any = WA.onEnterZone('sport1', () => {
+    WA.openPopup('popup_sport1', 'Bienvenue dans la salle solo : sport !', [{
+        label: "Merci !",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }])
+  })
+  
+  WA.onLeaveZone('sport1', () => {
+    helloWorldPopup.close();
+  });
+
+  let helloWorldPopup9: any = WA.onEnterZone('sport1', () => {
+    WA.openPopup('popup_sport1', 'Bienvenue dans la salle solo : sport !', [{
+        label: "Merci !",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }])
+  })
+  
+  WA.onLeaveZone('sport1', () => {
+    helloWorldPopup.close();
+  });
+
+  let helloWorldPopup10: any = WA.onEnterZone('sport1', () => {
+    WA.openPopup('popup_sport1', 'Bienvenue dans la salle solo : sport !', [{
+        label: "Merci !",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }])
+  })
+  
+  WA.onLeaveZone('sport1', () => {
+    helloWorldPopup.close();
+  });
+
+  let helloWorldPopup11: any = WA.onEnterZone('sport1', () => {
+    WA.openPopup('popup_sport1', 'Bienvenue dans la salle solo : sport !', [{
+        label: "Merci !",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }])
+  })
+  
+  WA.onLeaveZone('sport1', () => {
+    helloWorldPopup.close();
+  });
+
+  let helloWorldPopup12: any = WA.onEnterZone('sport1', () => {
+    WA.openPopup('popup_sport1', 'Bienvenue dans la salle solo : sport !', [{
+        label: "Merci !",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }])
+  })
+  
+  WA.onLeaveZone('sport1', () => {
+    helloWorldPopup.close();
+  });
+
+  let helloWorldPopup13: any = WA.onEnterZone('sport1', () => {
+    WA.openPopup('popup_sport1', 'Bienvenue dans la salle solo : sport !', [{
+        label: "Merci !",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }])
+  })
+  
+  WA.onLeaveZone('sport1', () => {
+    helloWorldPopup.close();
+  });
+
+  let helloWorldPopup14: any = WA.onEnterZone('sport1', () => {
+    WA.openPopup('popup_sport1', 'Bienvenue dans la salle solo : sport !', [{
+        label: "Merci !",
+        className: "primary",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+        }
+    }])
+  })
+  
+  WA.onLeaveZone('sport1', () => {
+    helloWorldPopup.close();
+  });
